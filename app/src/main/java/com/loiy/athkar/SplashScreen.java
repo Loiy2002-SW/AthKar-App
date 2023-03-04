5f70bca58a0153707fe3b373fb2bc111986f747c
Hassan Ayman <hsankraz66@gmail.com>
2023-03-04 15:33:29 +0300

    ADD SplashScreen

---
+++ app/src/main/java/com/loiy/athkar/SplashScreen.java
@@ -0,0 +1,46 @@
+package com.loiy.athkar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    //Declaration
+    Animation animationZoom;
+    ImageView imageView;
+    Handler handler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // remove the title from splash screen
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+
+        animationZoom = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom);
+        imageView = findViewById(R.id.splash_background_imageview);
+        imageView.startAnimation(animationZoom);
+
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                    //go to MainActivity
+                    Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(i);
+                    finish();
+            }
+        }, 4000);
+
+
+    }
+}
