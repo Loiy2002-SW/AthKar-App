dda2ccea146d9486782bf7e471c0f093760ca2b0
loiy al-hasan <lloiy9310@gmail.com>
2023-03-05 21:26:11 +0300

    FIX some bugs

--- app/src/main/java/com/loiy/athkar/SplashScreen.java
+++ app/src/main/java/com/loiy/athkar/SplashScreen.java
@@ -39,7 +39,7 @@ public class SplashScreen extends AppCompatActivity {
                     startActivity(i);
                     finish();
             }
-        }, 4000);
+        }, 3000);
 
 
     }
